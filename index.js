c3c695cf82bd25f762ab08276818b8b02bd1484a
Avi Cohen <avic@insighting.io>
2022-06-27 22:20:52 +0300

    done

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const events = [
   },
   {
     userId: 1,
-    place: "Wormholes Allow Information to Escape Black Holes",
-    name: "Check out this recent discovery about workholes",
+    place: "Wormholes Allow Information to Escape Black Holes 2",
+    name: "Check out this recent discovery about workholes 2",
     date: "2020-06-26T17:58:57.776Z",
     id: 2,
   },
@@ -37,8 +37,24 @@ app.get("/events", (req, res) => {
 });
 app.post("/form", (req, res) => {
   console.log(req.body);
-  const {value} = req.body
+  const { value } = req.body;
   res.send("success with value:" + value);
+});
+app.post("/event/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(events.find((event) => event.id === parseInt(id)).name);
+});
+app.get("/pie", (req, res) => {
+  randArr = [
+    parseInt(Math.random() * 100),
+    parseInt(Math.random() * 100),
+    parseInt(Math.random() * 100),
+    parseInt(Math.random() * 100),
+    parseInt(Math.random() * 100),
+    parseInt(Math.random() * 100),
+  ];
+
+  res.send(randArr);
 });
 
 app.listen(PORT, console.log("server is running on port #:", PORT));
